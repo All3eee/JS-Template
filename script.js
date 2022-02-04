75d9c16c7e45c17c11976cfd02a2e8d544cd1e15
All3eee <alvin.havren@skola.taby.se>
2022-02-04 11:09:21 +0100

    file update

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-const myString = "Hello, world!";
-console.log(myString);
+let pengar = Number(prompt('Hur mycket pengar har du?'));
+
+let järnsmycke = 200;
+let silversmycke = 500;
+let guldsmycke = 800;
+let järnsmycke_amount = 0;
+let silversmycke_amount = 0;
+let guldsmycke_amount = 0;
+
+let i = 0;
+
+while(i < 1) {
+    if (pengar >= guldsmycke) {
+        guldsmycke_amount += 1;
+        pengar -= guldsmycke;
+    } else if (pengar >= silversmycke) {
+        silversmycke_amount += 1;
+        pengar -= silversmycke;
+    } else if (pengar >= järnsmycke) {
+        järnsmycke_amount += 1;
+        pengar -= järnsmycke;
+    } else if (pengar < 200) {
+        console.log(`Du har råd med ${guldsmycke_amount} guldsmycken, ${silversmycke_amount} silversmycken och ${järnsmycke_amount} järnsmycken`)
+        i += 1;
+    }
+}
